6c794e5ccb79d65c5a7c670563938a031da57ca3
Chris Ford <christophertford@gmail.com>
2021-07-26 08:40:50 +0200

    Rip the name from the hash fragment.

--- src/bienvenides/events.cljs
+++ src/bienvenides/events.cljs
@@ -2,9 +2,14 @@
   (:require
    [re-frame.core :as re-frame]
    [bienvenides.db :as db]
-   ))
+   [clojure.string :as str]))
 
 (re-frame/reg-event-db
- ::initialize-db
- (fn [_ _]
-   db/default-db))
+  ::initialize-db
+  (fn [_ _]
+    (let [raw (if-let [hash js/window.location.hash]
+                (subs hash 1)
+                "")
+          name (str/split raw #"%20")]
+      (-> db/default-db
+          (assoc :name name)))))
