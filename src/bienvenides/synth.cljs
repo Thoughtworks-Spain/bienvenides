e01bf239e91e364914118360b59b96ac2a84eb63
Chris Ford <christophertford@gmail.com>
2021-08-08 20:45:39 +0200

    Add bass.

--- src/bienvenides/synth.cljs
+++ src/bienvenides/synth.cljs
@@ -14,13 +14,17 @@
     (cljb/percussive 0.01 1.4)
     (cljb/gain 0.1)))
 
+(def bass
+  (->> (melody/phrase [2 2 4] [0 -1 -2])
+       (melody/where :pitch (scale/from -5))))
+
 (defn concrete [notes]
   (->> notes
        (melody/tempo (melody/bpm 100))
        (melody/where :pitch (comp temperament/equal scale/C scale/major))))
 
 (defn play [notes audio-context]
-  (doseq [{:keys [pitch time duration] :as note} (concrete notes)]
+  (doseq [{:keys [pitch time duration] :as note} (concrete (melody/with notes bass))]
     (let [connected (cljb/connect-> (ping pitch) cljb/destination)
           at (+ time (cljb/current-time audio-context))]
       (cljb/run-with connected audio-context at duration))))
