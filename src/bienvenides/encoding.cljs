7c2d7b25db14dae4497af2e0385e93156cf78a0c
Chris Ford <christophertford@gmail.com>
2021-08-08 18:45:53 +0200

    Port a simple version of the encoding to CLJS.

---
+++ src/bienvenides/encoding.cljs
@@ -0,0 +1,26 @@
+(ns bienvenides.encoding
+  (:require [leipzig.melody :as melody]
+            [leipzig.scale :as scale]
+            [clojure.string :as str]
+            [cljs.pprint :as pprint]
+            ))
+
+(defn encode-pitch [persons-name]
+  (letfn [(index [c] (- (pprint/char-code c) (pprint/char-code \a)))]
+    (->> persons-name
+      (map index)
+      (map #(mod % 5)))))
+
+(defn encode-duration [persons-name]
+  (map (constantly 1) persons-name))
+
+(defn encode-one [persons-name]
+  (let [pitches (encode-pitch persons-name)
+        durations (encode-duration persons-name)]
+    (melody/phrase durations pitches)))
+
+(defn encode [names]
+  (->> names
+       (map str/lower-case)
+       (map encode-one)
+       (reduce melody/with)))
---
+++ test/bienvenides/encoding_test.cljs
@@ -0,0 +1,37 @@
+(ns bienvenides.encoding-test
+  (:require
+    [cljs.test :refer [deftest is]]
+    [bienvenides.encoding :as encoding]))
+
+(deftest initializes-to-empty
+  (is (= []
+         (encoding/encode []))))
+
+(deftest starts-at-a
+  (is (= [{:pitch 0 :duration 1 :time 0}
+          {:pitch 1 :duration 1 :time 1}
+          {:pitch 2 :duration 1 :time 2}]
+         (encoding/encode ["abc"]))))
+
+(deftest cycles-around-at-f
+  (is (= [{:pitch 4 :duration 1 :time 0}
+          {:pitch 0 :duration 1 :time 1}
+          {:pitch 1 :duration 1 :time 2}]
+         (encoding/encode ["efg"]))))
+
+(deftest treats-multiple-words-as-multiple-parts
+  (is (= [
+          {:pitch 0 :duration 1 :time 0}
+          {:pitch 2 :duration 1 :time 0}
+          {:pitch 4 :duration 1 :time 0}
+
+          {:pitch 1 :duration 1 :time 1}
+          {:pitch 3 :duration 1 :time 1}
+          {:pitch 0 :duration 1 :time 1}]
+         (encoding/encode ["ab" "cd" "ef"]))))
+
+(deftest treats-uppercase-the-same-as-lowercase
+  (is (= [{:pitch 0 :duration 1 :time 0}
+          {:pitch 1 :duration 1 :time 1}
+          {:pitch 2 :duration 1 :time 2}]
+         (encoding/encode ["ABC"]))))
