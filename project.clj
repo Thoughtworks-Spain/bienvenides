56e61c13625e0caa42e5183e4744f52140e62660
Chris Ford <christophertford@gmail.com>
2021-08-08 21:14:59 +0200

    Remove server-side prototype.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject bienvenides "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [overtone "0.10.3"]
                  [leipzig "0.11.0-SNAPSHOT"]
                  [cljs-bach "0.3.0-SNAPSHOT"]])
