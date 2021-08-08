95962e3309edc2a7fb1043f510a2276357b9c182
Chris Ford <christophertford@gmail.com>
2021-08-08 21:14:20 +0200

    Repeat the first bar three times.

--- src/bienvenides/synth.cljs
+++ src/bienvenides/synth.cljs
@@ -18,13 +18,17 @@
   (->> (melody/phrase [2 2 4] [0 -1 -2])
        (melody/where :pitch (scale/from -5))))
 
-(defn concrete [notes]
-  (->> notes
+(defn arrange [notes]
+  (let [once (->> notes (melody/with bass))]
+    (->>
+       (melody/cut 4 once)
+       (melody/times 2)
+       (melody/with (melody/after 8 once))
        (melody/tempo (melody/bpm 100))
-       (melody/where :pitch (comp temperament/equal scale/C scale/major))))
+       (melody/where :pitch (comp temperament/equal scale/C scale/pentatonic)))))
 
 (defn play [notes audio-context]
-  (doseq [{:keys [pitch time duration] :as note} (concrete (melody/with notes bass))]
+  (doseq [{:keys [pitch time duration] :as note} (arrange notes)]
     (let [connected (cljb/connect-> (ping pitch) cljb/destination)
           at (+ time (cljb/current-time audio-context))]
       (cljb/run-with connected audio-context at duration))))
