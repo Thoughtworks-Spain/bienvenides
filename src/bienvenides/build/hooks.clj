1c048b9d99284bf755165f19e6e8760bbb5cf0ba
Vitor Quintanilha Barbosa <vitorbarbosa@Vitors-MBP.home>
2021-08-17 09:15:17 +0200

    Adds scss compilation using shadow-cljs hooks and sass

--- src/bienvenides/build/hooks.clj
+++ src/bienvenides/build/hooks.clj
@@ -9,12 +9,42 @@
 (defn generate-index
   "Generates an `index.html` file at the public folder with the correct entrypoint url based on a template"
   {:shadow.build/stage :flush}
-  [build-state entrypoint-url destination-folder]
-  (let [dest-file (str destination-folder "/index.html")]
+  [build-state & _]
+  (let [entrypoint-url (-> build-state :shadow.build/config :asset-path (str "/app.js"))
+        stylesheet-url (-> build-state :shadow.build/config :bienvenides/stylesheet-url)
+        target-dir (-> build-state :shadow.build/config :bienvenides/target-dir)
+        dest-file (str target-dir "/index.html")]
     (prn (str "Writting file " dest-file " with entrypoint " entrypoint-url))
+    (io/make-parents dest-file)
     (with-open [reader (io/reader INDEX_TEMPLATE)]
       (with-open [writer (io/writer dest-file)]
         (doseq [line (line-seq reader)]
-          (.write writer (string/replace line "{{ENTRYPOINT_URL}}" entrypoint-url))
+          (.write writer (-> line
+                             (string/replace "{{ENTRYPOINT_URL}}" entrypoint-url)
+                             (string/replace "{{STYLESHEET_URL}}" stylesheet-url)))
           (.write writer "\n"))))
+    build-state))
+
+(defn compile-scss
+  "Compiles all scss. Only used for production"
+  {:shadow.build/stage :flush}
+  [build-state & _]
+  (let [target-dir (-> build-state :shadow.build/config :bienvenides/target-dir)
+        cmd ["npx" "sass" "./src/scss/core.scss" (str target-dir "/css/compiled/index.css")]]
+    (->> cmd (string/join " ") (str "Calling command: ") prn)
+    (let [{:keys [exit out err] :as sh-result} (apply shell/sh cmd)]
+      (when (not= exit 0)
+        (throw (ex-info "Failed to compile scss to css" {:sh-result sh-result}))))
+    build-state))
+
+(defn watch-scss
+  "Compiles scss, trigering recompilation on changes. Only used for
+  dev. Notice that by relying on `:configure` stage this hook only run
+  once during `shadow-cljs watch`.."
+  {:shadow.build/stage :configure}
+  [build-state & _]
+  (let [target-dir (-> build-state :shadow.build/config :bienvenides/target-dir)
+        cmd ["npx" "sass" "--watch" "./src/scss/core.scss" (str target-dir "/css/compiled/index.css")]]
+    (->> cmd (string/join " ") (str "Calling command: ") prn)
+    (-> cmd ProcessBuilder. .inheritIO .start)
     build-state))
